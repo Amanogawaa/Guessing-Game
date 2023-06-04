50065715c3b11889d184afc7dcda05a903fbb66f
Dominic Molino <dommolino28@gmail.com>
2023-06-04 10:37:40 +0800

    Simple Guessing Game

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Random random = new Random();
+        int secret = random.Next(1, 20);
+        int guess = 0;
+        int guessCount = 0;
+        int guessLimit = 5;
+        bool outofGuess = false;
+
+        while (guess != secret && !outofGuess)
+        {
+            if (guessCount < guessLimit)
+            {
+                Console.WriteLine($"You only have {guessLimit - guessCount}");
+                Console.Write("Enter you guess: ");
+                guess = Convert.ToInt32(Console.ReadLine());
+                guessCount++;
+            }
+            else
+            {
+                outofGuess = true;
+            }
+        }
+        if (outofGuess)
+        {
+            Console.WriteLine("Bobo ka!");
+        }
+        else
+        {
+            Console.WriteLine("You've guessed it!");
+        }
+        //prints the secret number
+        Console.Write($"Secret Number: {secret}");
+    }
+}
